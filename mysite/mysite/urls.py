c3fb79054b4f0536fc40c0720768adf05433e781
Timo Lubitz <timo.lubitz@gmail.com>
2019-02-28 13:51:14 +0100

    adding urls, static pages, first CSS

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,9 @@
+from . import views
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -21,9 +24,9 @@ urlpatterns = [
     # the list:
     url(r'', include(wagtail_urls)),
 
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
+    #static stuff
+    path('lodge', views.lodge, name='lodge'),
+    path('contact', views.contact, name='contact'),
 ]
 
 
